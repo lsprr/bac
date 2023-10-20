b65dc5417b78e8aecb97abab4c45b6b605fc2ee7
lsprr <16653744+lsprr@users.noreply.github.com>
2023-10-20 19:25:38 -0400

    Add a tracking number and external tracking number right above the timeline

--- frontend/src/pages/tracking.tsx
+++ frontend/src/pages/tracking.tsx
@@ -22,7 +22,7 @@ export default function TrackingView() {
         <section className='mt-10'>
             <Header />
             <SearchForm trackingNumber={trackingNumber} setTrackingNumber={setTrackingNumber} handleTracking={handleTracking} onHandleSubmit={handleSubmit} />
-            {trackingInfo && <TrackingStatusList items={trackingInfo['parcel_tracking_items']} />}
+            {trackingInfo && <TrackingStatusList labels={trackingInfo['label']} items={trackingInfo['parcel_tracking_items']} />}
         </section >
     )
 }
@@ -61,13 +61,26 @@ function SearchForm({ trackingNumber, setTrackingNumber, handleTracking, onHandl
     );
 }
 
-function TrackingStatusList({ items }) {
+function TrackingStatusList({ items, labels }) {
+    const trackingNumber = labels?.tracking_number;
+    const externalTrackingNumber = labels?.external_tracking_number;
+
     return (
-        <ol className="mt-3 divide-y divider-gray-200 pt-10">
-            {items.map(item => (
-                <TrackingStatusItem key={item.uid} item={item} />
-            ))}
-        </ol>
+        <>
+            <div className='pt-10 flex flex-col md:flex-row justify-between'>
+                <p>
+                    <b>Tracking Number:</b> {trackingNumber}
+                </p>
+                <p>
+                    <b>External Tracking Number:</b> {externalTrackingNumber}
+                </p>
+            </div>
+            <ol className="mt-3 divide-y divider-gray-200">
+                {items.map(item => (
+                    <TrackingStatusItem key={item.uid} item={item} />
+                ))}
+            </ol>
+        </>
     );
 }
 
@@ -79,41 +92,35 @@ function TrackingStatusItem({ item }) {
     const location = item.location ?? item.city + ', ' + item.state;
 
     return (
-        <>
-            <li>
-                <div className="items-center block py-3 sm:flex">
-                    <div className="text-gray-800">
-
-                        <time className="text-lg font-semibold">{day}</time>
-                        <div className="text-base font-normal flex items-center">
-                            <span className="text-gray-800 capitalize">
-                                {description}
-                            </span>
-                            <span className="inline-flex items-center justify-center w-4 h-4 ml-2 text-sm font-semibold rounded-full bg-green-700 text-green-300">
-                                <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
-                                </svg>
-                                <span className="sr-only">Icon description</span>
-                            </span>
-                        </div>
-
-                        <span className="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2 border border-gray-500">
-                            <svg className="w-2.5 h-2.5 mr-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
-                            </svg>
-                            {time}
+        <li>
+            <div className="items-center block py-3 sm:flex">
+                <div className="text-gray-800">
+                    <time className="text-lg font-semibold">{day}</time>
+                    <div className="text-base font-normal flex items-center">
+                        <span className="text-gray-800 capitalize">
+                            {description}
                         </span>
-
-                        <span className="uppercase bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded border border-blue-600">
-                            <svg className="w-2.5 h-2.5 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M10 .5a9.5 9.5 0 1 0 0 19 9.5 9.5 0 0 0 0-19ZM8.374 17.4a7.6 7.6 0 0 1-5.9-7.4c0-.83.137-1.655.406-2.441l.239.019a3.887 3.887 0 0 1 2.082 2.5 4.1 4.1 0 0 0 2.441 2.8c1.148.522 1.389 2.007.732 4.522Zm3.6-8.829a.997.997 0 0 0-.027-.225 5.456 5.456 0 0 0-2.811-3.662c-.832-.527-1.347-.854-1.486-1.89a7.584 7.584 0 0 1 8.364 2.47c-1.387.208-2.14 2.237-2.14 3.307a1.187 1.187 0 0 1-1.9 0Zm1.626 8.053-.671-2.013a1.9 1.9 0 0 1 1.771-1.757l2.032.619a7.553 7.553 0 0 1-3.132 3.151Z" />
+                        <span className="inline-flex items-center justify-center w-4 h-4 ml-2 text-sm font-semibold rounded-full bg-green-700 text-green-300">
+                            <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
                             </svg>
-                            {location}
+                            <span className="sr-only">Icon description</span>
                         </span>
-
                     </div>
+                    <span className="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2 border border-gray-500">
+                        <svg className="w-2.5 h-2.5 mr-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
+                        </svg>
+                        {time}
+                    </span>
+                    <span className="uppercase bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded border border-blue-600">
+                        <svg className="w-2.5 h-2.5 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 0 19 9.5 9.5 0 0 0 0-19ZM8.374 17.4a7.6 7.6 0 0 1-5.9-7.4c0-.83.137-1.655.406-2.441l.239.019a3.887 3.887 0 0 1 2.082 2.5 4.1 4.1 0 0 0 2.441 2.8c1.148.522 1.389 2.007.732 4.522Zm3.6-8.829a.997.997 0 0 0-.027-.225 5.456 5.456 0 0 0-2.811-3.662c-.832-.527-1.347-.854-1.486-1.89a7.584 7.584 0 0 1 8.364 2.47c-1.387.208-2.14 2.237-2.14 3.307a1.187 1.187 0 0 1-1.9 0Zm1.626 8.053-.671-2.013a1.9 1.9 0 0 1 1.771-1.757l2.032.619a7.553 7.553 0 0 1-3.132 3.151Z" />
+                        </svg>
+                        {location}
+                    </span>
                 </div>
-            </li>
-        </>
+            </div>
+        </li>
     );
 }
