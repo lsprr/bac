a377361b6749927e34c49a7b1896100bd33066a8
lsprr <16653744+lsprr@users.noreply.github.com>
2023-10-20 14:08:05 -0400

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3001;
+const SECRET = process.env.JWT_SECRET;
+const TOKEN = process.env.BEARER_TOKEN;
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/generate_token', (req, res) => {
+    const { login, password } = req.body;
+    const token = jwt.sign({ login, password }, SECRET);
+    res.json({ token });
+});
+
+app.get('/tracking_parcel', async (req, res) => {
+    const { tracking_number } = req.query;
+    try {
+        const response = await axios.get(`https://bps.bringer.io/public/api/v2/get/parcel/tracking.json?tracking_number=${tracking_number}`, {
+            headers: {
+                Authorization: TOKEN
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to retrieve parcel info' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
