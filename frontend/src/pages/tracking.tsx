575ce73c7715ed389e45c553e8eddba74e679f2a
lsprr <16653744+lsprr@users.noreply.github.com>
2023-10-20 23:07:16 -0400

    Implement loader animation on button click

--- frontend/src/pages/tracking.tsx
+++ frontend/src/pages/tracking.tsx
@@ -41,6 +41,7 @@ interface SearchFormProps {
 const TrackingView = () => {
     const [trackingNumber, setTrackingNumber] = useState<string>('');
     const [trackingInfo, setTrackingInfo] = useState<any | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -53,8 +54,10 @@ const TrackingView = () => {
         }
 
         try {
+            setLoading(true);
             const response = await axios.get(`http://localhost:3001/tracking_parcel?tracking_number=${trackingNumber}`);
             setTrackingInfo(response.data);
+            setLoading(false);
         } catch (error) {
             console.error('Error fetching tracking info:', error);
         }
@@ -63,7 +66,7 @@ const TrackingView = () => {
     return (
         <section className='mt-10'>
             <Header />
-            <SearchForm trackingNumber={trackingNumber} setTrackingNumber={setTrackingNumber} handleTracking={handleTracking} onHandleSubmit={handleSubmit} />
+            <SearchForm trackingNumber={trackingNumber} setTrackingNumber={setTrackingNumber} handleTracking={handleTracking} onHandleSubmit={handleSubmit} loading={loading} />
             {trackingInfo && <TrackingStatusList labels={trackingInfo['label']} items={trackingInfo['parcel_tracking_items']} />}
         </section >
     )
@@ -80,7 +83,7 @@ const Header = () => {
     );
 }
 
-const SearchForm = ({ trackingNumber, setTrackingNumber, handleTracking, onHandleSubmit }: SearchFormProps) => {
+const SearchForm = ({ trackingNumber, setTrackingNumber, handleTracking, onHandleSubmit, loading }: SearchFormProps) => {
     return (
         <form onSubmit={onHandleSubmit}>
             <div className='mb-5'>
@@ -96,7 +99,18 @@ const SearchForm = ({ trackingNumber, setTrackingNumber, handleTracking, onHandl
                 />
             </div>
             <div className='flex justify-between md:justify-start'>
-                <button onClick={handleTracking} className="mr-5 inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">Track Parcel</button>
+                {loading
+                    ?
+                    <button disabled type="button" className="mr-5 inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
+                        <svg aria-hidden="true" role="status" className="inline w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                        </svg>
+                    </button>
+                    :
+                    <button onClick={handleTracking} className="mr-5 inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">Track Parcel</button>
+
+                }
                 <button className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none" disabled>Show Map</button>
             </div>
         </form>
